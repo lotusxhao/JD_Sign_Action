efbd419bc84c4c9daf083f5103e8a1fdda9615ca
lotusxhao <lotusxhao@users.noreply.github.com>
2021-01-30 23:21:00 +0800

    Update jd_sign_DD.js

--- jd_sign_DD.js
+++ jd_sign_DD.js
@@ -8,7 +8,7 @@ const rp = require('request-promise')
 const download = require('download')
 
 // 京东cookie
-const c = process.env.JD_COOKIE_DD
+const cookieDD = process.env.JD_COOKIE_DD
 // Server酱SCKEY
 const push_key = process.env.PUSH_KEY
 
